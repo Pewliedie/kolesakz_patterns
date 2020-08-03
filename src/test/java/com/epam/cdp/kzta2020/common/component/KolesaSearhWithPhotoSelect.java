f7ddd180f3e158801d3c6816e64c0f5701089116
Vladislav Permyakov <vladislav_permyakov@epam.com>
2020-08-03 23:51:28 +0600

    refactoring

--- src/test/java/com/epam/cdp/kzta2020/common/component/KolesaSearhWithPhotoSelect.java
+++ src/test/java/com/epam/cdp/kzta2020/common/component/KolesaSearhWithPhotoSelect.java
@@ -4,6 +4,7 @@ import com.epam.cdp.kzta2020.pages.AbstractPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.pagefactory.ByChained;
 
 public class KolesaSearhWithPhotoSelect extends AbstractPage {
 
