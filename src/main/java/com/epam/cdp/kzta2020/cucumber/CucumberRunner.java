de7aa1d2958267c9a1a67ab627c16cf5a1fcf010
Vladislav Permyakov <vladislav_permyakov@epam.com>
2020-08-26 21:34:50 +0600

    refactor

--- src/main/java/com/epam/cdp/kzta2020/cucumber/CucumberRunner.java
+++ src/main/java/com/epam/cdp/kzta2020/cucumber/CucumberRunner.java
@@ -5,7 +5,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 
-@CucumberOptions(features = "src/test/resources/cucumber_features/car-search.feature"
+@CucumberOptions(features = "src/test/resources/cucumber_features/"
         , glue = {"com.epam.cdp.kzta2020.cucumber.step_definitions"})
 public class CucumberRunner extends AbstractTestNGCucumberTests {
 }
